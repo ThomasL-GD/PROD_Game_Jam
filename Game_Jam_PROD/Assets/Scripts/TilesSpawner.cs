ab57d79256b8c34156eb24d299cd7083f2c5ceb7
Max-Martin-Sherie <59953355+Max-Martin-Sherie@users.noreply.github.com>
2020-12-29 19:23:41 +0100

    V0.7
    3 room types

---
+++ Game_Jam_PROD/Assets/Scripts/TilesSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TilesSpawner : MonoBehaviour
+{
+    [SerializeField] GameObject[] m_tiles = null;
+    [SerializeField] int[] m_tileDifficultyMin = null;
+    [SerializeField][Tooltip("0 for infinity")] int[] m_tileDifficultyMax = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        bool spawning = true;
+        while (spawning)
+        {
+            int random = Random.Range(0, m_tiles.Length);
+            if (m_tileDifficultyMin[random] <= SpawnController.m_playerFloor && (m_tileDifficultyMax[random] > SpawnController.m_playerFloor || m_tileDifficultyMax[random] == 0))
+            {
+                Instantiate(m_tiles[random], transform.position, Quaternion.Euler(-90, 45 * Random.Range(0, 9), 0));
+                spawning = false;
+                Destroy(gameObject);
+            }
+            
+        }
+    }
+}
